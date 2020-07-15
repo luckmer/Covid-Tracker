bf09c8e73862b8c5c664381b645d24efba006dd3
luckmer <woszczo04@gmail.com>
2020-07-15 11:31:50 +0200

    Work panel

---
+++ src/Components/GlobalStyle/index.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const globalStyle = createGlobalStyle`
+
+html,body{
+    padding:0;
+    margin:0;
+    background-color: #161616;
+    
+    ${"" /* // */}
+    overflow-x:hidden;
+}
+
+`;
+
+export default globalStyle;
